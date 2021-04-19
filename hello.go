e054d2113babdd31c1b9b670ce7cccce9418e3e4
yskszk63 <yskszk63@gmail.com>
2021-04-19 11:55:48 +0900

    test background sleep

--- hello.go
+++ hello.go
@@ -3,9 +3,17 @@ package main
 import (
     "github.com/aws/aws-lambda-go/events"
     "github.com/aws/aws-lambda-go/lambda"
+    "fmt"
+    "time"
 )
 
+func background() {
+    time.Sleep(time.Second * 10)
+    fmt.Println("OK")
+}
+
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+    go background()
     return &events.APIGatewayProxyResponse {
         StatusCode: 200,
         Body: "Hello, World!",
